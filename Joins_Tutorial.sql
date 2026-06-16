9f75f88c44df2ccdc84cb4f95671b32a454c976b
Pavan Sai Kalyan <alanepavansaikalyan3105@gmail.com>
2026-06-16 08:29:21 +0530

    Joins added

--- Joins_Tutorial.sql
+++ Joins_Tutorial.sql
@@ -30,3 +30,41 @@ from employee_salary e
 join employee_salary s
 	on e.employee_id=s.employee_id
     ;
+    
+    
+#--Join Multiple tables
+SELECT * 
+FROM employee_demographics e
+inner join employee_salary s
+	on e.employee_id = s.employee_id
+inner join parks_departments p
+	on p.department_id = s.dept_id;
+    
+#--UNION and UNION All
+SELECT first_name, last_name
+from employee_demographics 
+union 
+select age, gender
+from employee_demographics;
+
+# the above query merges first name with age and last name with gender and ths cause data inconsistency..
+
+SELECT first_name, last_name
+from employee_demographics
+union #gives unique union values ..to get all the values just use union all.
+select first_name, last_name
+from employee_demographics;
+
+SELECT first_name, last_name,'old_man' as label
+from employee_demographics
+where age>40 AND gender ='Male'
+union
+select first_name,last_name,'old_women' as label
+from employee_demographics
+where age>40 AND gender='Female'
+union
+select first_name, last_name ,'High Paid' as label
+from employee_salary
+where salary>70000
+order by first_name,last_name
+;
